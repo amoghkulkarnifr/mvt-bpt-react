c3eca37b4462d68ab2b071e6dea9c9bd57c33358
amoghkulkarnifr <amogh.kulkarni@fradium.com>
2023-08-24 13:52:10 +0530

    Added configurability for width and data

--- react_component/mvt-bpt-component/src/components/Table/Table.tsx
+++ react_component/mvt-bpt-component/src/components/Table/Table.tsx
@@ -8,6 +8,8 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
+import Papa from 'papaparse';
+
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.grey[500],
@@ -22,56 +24,51 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
     backgroundColor: theme.palette.action.hover,
   },
-  // hide last border
-  // '&:last-child td, &:last-child th': {
-  //   border: 0,
-  // },
 }));
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
+interface BasicTableProps {
+  minWidth?: number,
+  header: string[],
+  rows: string[][]
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-export default function BasicTable() {
+const BasicTable: React.FC<BasicTableProps> = ({ minWidth=350, header, rows }) => {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table 
+        sx={{ minWidth: minWidth }} 
+        aria-label="simple table">
+        {/* Table header */}
         <TableHead>
           <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+            {
+              header.map((cell, cIndex) => (
+                <StyledTableCell
+                  key={cIndex}
+                  align={cIndex == 0? undefined: 'right'}>
+                  {cell}
+                </StyledTableCell>
+              ))
+            }
           </TableRow>
         </TableHead>
+
+        {/* Table rows */}
         <TableBody>
-          {rows.map((row) => (
+          {rows.map((row, rIndex) => (
             <StyledTableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              key={rIndex}
             >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              {
+                row.map((cell, cIndex) => (
+                  <TableCell 
+                    component={cIndex == 0? "th": undefined} 
+                    align={cIndex == 0? undefined: 'right'}
+                    scope="row">
+                    {cell}
+                  </TableCell>
+                ))
+              }
             </StyledTableRow>
           ))}
         </TableBody>
@@ -79,3 +76,5 @@ export default function BasicTable() {
     </TableContainer>
   );
 }
+
+export default BasicTable;
--- react_component/mvt-bpt-component/src/stories/Table.stories.ts
+++ react_component/mvt-bpt-component/src/stories/Table.stories.ts
@@ -12,11 +12,40 @@ const meta = {
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
+
+  argTypes: {
+    minWidth: {
+      control: 'number',
+      description: 'Minimum width of a table column'
+    },
+    header: {
+      control: 'object',
+      description: 'Header of the table'
+    },
+    rows: {
+      control: 'object',
+      description: 'Rows of the table'
+    }
+  },
+
+  args: {
+    header: ['Column 1', 'Column 2', 'Column 3'],
+    rows: [
+      ['Value 1_1', 'Value 1_2', 'Value 1_3'],
+      ['Value 2_1', 'Value 2_2', 'Value 2_3'],
+      ['Value 3_1', 'Value 3_2', 'Value 3_3'],
+    ],
+    minWidth: 300,
+  }
 } satisfies Meta<typeof BasicTable>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Default: Story = {};
 
-}
+export const Wide: Story = {
+  args: {
+    minWidth: 600
+  }
+};
