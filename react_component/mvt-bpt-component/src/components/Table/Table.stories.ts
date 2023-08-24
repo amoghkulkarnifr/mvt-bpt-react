9e84e8c4947738b5471a0d492240ab0a4bd149ab
amoghkulkarnifr <amogh.kulkarni@fradium.com>
2023-08-24 19:01:50 +0530

    Rearranged component and story files

--- react_component/mvt-bpt-component/src/components/Table/Table.stories.ts
+++ react_component/mvt-bpt-component/src/components/Table/Table.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import BasicTable from '../components/Table/Table';
+import BasicTable from './Table';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
--- react_component/mvt-bpt-component/src/components/Table/Table.tsx
+++ react_component/mvt-bpt-component/src/components/Table/Table.tsx
@@ -8,8 +8,6 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-import Papa from 'papaparse';
-
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.grey[500],
